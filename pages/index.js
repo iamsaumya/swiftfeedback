2ebecdc193575662051836779d326b545e66f516
Saumya Pandey <sp160899@gmail.com>
2020-09-12 20:00:46 +0530

    Add logo to the theme and beautify homepage

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
 import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
-import { Button, Heading, Text, Code } from '@chakra-ui/core';
+import { Button, Heading, Text, Code, Icon, Flex } from '@chakra-ui/core';
 const Home = () => {
   const auth = useAuth();
   return (
-    <div className="container">
-      <Head>Swift Feedback</Head>
-      <main>
-        <Heading fontWeight="bold">Swift Feedback</Heading>
-        <Button onClick={() => auth.signinwithGithub()}>Sign In</Button>
-        <Text>
-          Current User: <Code>{auth?.user?.email}</Code>
-        </Text>
-        {auth?.user && <Button onClick={() => auth.signout()}>Sign out</Button>}
-      </main>
-    </div>
+    <Flex align="center" justify="center" direction="column" minHeight="100vh">
+      <Head>
+        <title>Swift Feedback</title>
+      </Head>
+
+      <Icon name="logo" size="64px" />
+      <Button onClick={() => auth.signinwithGithub()}>Sign In</Button>
+      {auth?.user && <Button onClick={() => auth.signout()}>Sign out</Button>}
+    </Flex>
   );
 };
 
